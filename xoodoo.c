5db102beb8e24d1f7b5917ca9160c38fc06299e2
luiz <luiz@sdf.org>
2023-01-10 14:09:14 -0300

    fix xoodoo

--- xoodoo.c
+++ xoodoo.c
@@ -1,17 +1,13 @@
 #include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 
 #define ROTR32(x, b) (((x) >> (b)) | ((x) << (32 - (b))))
-#define SWAP32(s, u, v) t = (s)[u], (s)[u] = (s)[v], (s)[v] = t
+#define SWAP(s, u, v) t = (s)[u], (s)[u] = (s)[v], (s)[v] = t
 
 void xoodoo(uint32_t state[12])
 {
     uint32_t e[4], a, b, c, t, r, i;
-    const uint32_t k[12] = {
-        0x058, 0x038, 0x3C0, 0x0D0, 0x120, 0x014,
-        0x060, 0x02C, 0x380, 0x0F0, 0x1A0, 0x012
-    };
+    const uint32_t k[12] = { 0x058, 0x038, 0x3C0, 0x0D0, 0x120, 0x014,
+                             0x060, 0x02C, 0x380, 0x0F0, 0x1A0, 0x012 };
 
     for (r = 0; r < 12; r++) {
         for (i = 0; i < 4; i++) {
@@ -19,9 +15,9 @@ void xoodoo(uint32_t state[12])
             e[i] ^= ROTR32(e[i], 9);
         }
         for (i = 0; i < 12; i++) state[i] ^= e[(i - 1) & 3];
-        SWAP32(state, 7, 4);
-        SWAP32(state, 7, 5);
-        SWAP32(state, 7, 6);
+        SWAP(state, 7, 4);
+        SWAP(state, 7, 5);
+        SWAP(state, 7, 6);
         state[0] ^= k[r];
         for (i = 0; i < 4; i++) {
             a = state[i];
@@ -31,15 +27,16 @@ void xoodoo(uint32_t state[12])
             state[i + 4] = ROTR32((a & ~c) ^ b, 31);
             state[i] ^= c & ~b;
         }
-        SWAP32(state, 8, 10);
-        SWAP32(state, 9, 11);
+        SWAP(state, 8, 10);
+        SWAP(state, 9, 11);
     }
 }
 
-void absorb(uint32_t state[12], const uint8_t *message, size_t size)
+void absorb(uint32_t state[12], const uint8_t *message, uint64_t size)
 {
-    size_t i;
     uint8_t *st = (uint8_t *) state;
+    uint64_t i;
+
     while (size >= 16) {
         for (i = 0; i < 16; i++) st[i] ^= message[i];
         xoodoo(state);
@@ -52,33 +49,39 @@ void absorb(uint32_t state[12], const uint8_t *message, size_t size)
     xoodoo(state);
 }
 
-void squeeze(uint32_t state[12], uint8_t *digest, size_t size)
+void squeeze(uint32_t state[12], uint8_t *digest, uint64_t size)
 {
-    size_t i;
     uint8_t *st = (uint8_t *) state;
+    uint64_t i;
+
     while (size >= 16) {
         for (i = 0; i < 16; i++) digest[i] = st[i];
         xoodoo(state);
         digest += 16;
         size -= 16;
     }
-    for (i = 0; i < size; i++) digest[i] = st[i];
-    xoodoo(state);
+    if (size > 0) {
+        for (i = 0; i < size; i++) digest[i] = st[i];
+        xoodoo(state);
+    }
 }
+
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
     size_t size, i;
-    FILE *tty = fopen("/dev/tty", "rb");
+    FILE *tty = fopen("/dev/tty", "r+");
     uint32_t state[12] = { 0 };
     char buffer[256] = { 0 };
 
     if (!tty) return 1;
 
-    fprintf(stderr, "Password: ");
+    fprintf(tty, "Key: ");
     fgets(buffer, sizeof(buffer), tty);
     buffer[strcspn(buffer, "\n")] = 0;
-    fprintf(stderr, "IV: ");
+    fprintf(tty, "Nonce: ");
     fgets(buffer + strlen(buffer), sizeof(buffer), tty);
     buffer[strcspn(buffer, "\n")] = 0;
     absorb(state, (uint8_t *) buffer, strlen(buffer));
